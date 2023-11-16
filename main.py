7fe4b22dd064ed4822cbfd65c7ceda21574d3f9b
Pabmepal <pabmepal@gmail.com>
2023-11-16 16:06:45 +0100

    Se ha creado una función más general para descargar datos por horas y resumirlos en media diaria

--- main.py
+++ main.py
@@ -172,35 +172,97 @@ def precio_energia_medio_dia(fecha='2022-11-09',cortetiempo='hour',limitgeo='pen
     cursor.close()
     print(f'terminado precio: {fecha}')
 
-def precio_energia_periodo(fechaini=None,fechafin=None):
+def datos_energia_periodo(fechaini=None,fechafin=None):
     if fechaini:
         pass
     else:
-        fechaini=fecha_ini(tabla='mercados_precios_mercados_tiempo_real')
+        fechaini1=fecha_ini(tabla='mercados_precios_mercados_tiempo_real')
+        fechaini2=fecha_ini(tabla='demanda_demanda_tiempo_real')
     
     if fechafin:
         pass
     else:
         fechafin=str(datetime.now().date()-timedelta(days=1))
 
-    fechaini=parse(fechaini).date()
+    fechaini1=parse(fechaini1).date()
+    fechaini2=parse(fechaini2).date()
     fechafin=parse(fechafin).date()
     
-    diasperiodo= (fechafin-fechaini).days+1
+    diasperiodo1= (fechafin-fechaini1).days+1
+    diasperiodo2= (fechafin-fechaini2).days+1
     
-    for dia in range(diasperiodo):
-        precio_energia_medio_dia(fechaini+timedelta(days=dia))
-        #print(fechaini+timedelta(days=dia))
-        
+    for dia in range(diasperiodo1):
+        dato_media_dia(fechaini1+timedelta(days=dia),category='mercados',widget='precios-mercados-tiempo-real')
+    
+    for dia in range(diasperiodo2):
+        dato_media_dia(fechaini2+timedelta(days=dia))
+def dato_media_dia(fecha='2022-11-09',cortetiempo='hour',limitgeo='peninsular', category='demanda',widget='demanda-tiempo-real',dropjson=False):
+    #Formateo de fecha
+    fechaini = f'{fecha}T00:00'
+    fechafin = f'{fecha}T23:59'
+    url = f'https://apidatos.ree.es/es/datos/{category}/{widget}'
+
+    query ={
+    'start_date' : fechaini,
+    'end_date' : fechafin,
+    'time_trunc' : cortetiempo,
+    'geo_limit' : limitgeo,
+    }
+    response = requests.get(url=url, params=query)
+
+    if response.status_code== requests.codes.ok: 
+        jsonresponse = response.json()
+
+        if dropjson:
+
+            with open(f'{category}_{widget}.json', 'w') as archivo_json:
+                json.dump(jsonresponse, archivo_json)
+        else:
+            conn, cursor = open_conn()
+            widget_mod=widget.replace('-','_')
+
+            tabla= f'{category}_{widget_mod}'
+            crear_tabla = '''CREATE TABLE IF NOT EXISTS {}(
+            fecha_reg DATE PRIMARY KEY,
+            {} FLOAT
+             )'''.format(
+                 ''.join(tabla),
+                 ' FLOAT, '.join([tipo['type'].replace(' ','_').replace('(€/MWh)','EMWH') for tipo in jsonresponse['included']])
+                 )
+            cursor.execute(crear_tabla)
+            conn.commit()
+
+            diccarga=dict()
+
+            for registros in jsonresponse['included']:
+                valortotal=0.
+                for valor in registros['attributes']['values']:
+                    valortotal+=valor['value']
+
+                diccarga.update({registros['type'].replace(' ','_').replace('(€/MWh)','EMWH') : valortotal/len(registros['attributes']['values'])})
+                
+            fecha=parse(fechaini).date()
+            diccarga['fecha_reg']= fecha
+                
+            
+            insert_sql_dic(tabla,diccarga,cursor,conn)
+            print(f'terminado recogida dato {category} para fecha: {fecha}')
+
+            conn.close()
+            cursor.close()
 
 
+
+
+
+           
 #Ejecuciones
 
-#estructura_generacion()
+estructura_generacion()
 
-#precio_energia_periodo()
+datos_energia_periodo()
 
-precio_energia_medio_dia(category='demanda',widget='demanda-tiempo-real',dropjson=True,cortetiempo='day')
+#demanda_media_dia()
 
 
 if __name__ == '__main__':
